bd1918ecd933a5a53842c65070fa3542f884ec15
wassabi-glitch <ahrorabrorov26@gmail.com>
2026-01-30 11:49:59 +0500

    Allow frontend CORS

--- app/main.py
+++ app/main.py
@@ -18,7 +18,10 @@ app = FastAPI(
 # TODO: Replace with your frontend domain(s) in production.
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "http://localhost:5173",
+        "http://127.0.0.1:5173",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
