5e50041b7553c23469ed1ba11bfce4f0beadc936
wassabi-glitch <ahrorabrorov26@gmail.com>
2026-03-30 05:05:13 +0500

    fix: debug entrypoint for networking

---
+++ frontend/entrypoint.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+echo "--- DEBUG: Current Nginx Config ---"
+cat /etc/nginx/conf.d/default.conf
+echo "--- END DEBUG ---"
+
+# Manual replacement of placeholders
+# We use | as a delimiter because the URL contains /
+sed -i "s|__API_URL__|$API_URL|g" /etc/nginx/conf.d/default.conf
+sed -i "s|__API_HOST__|$API_HOST|g" /etc/nginx/conf.d/default.conf
+
+echo "--- DEBUG: Modified Nginx Config ---"
+cat /etc/nginx/conf.d/default.conf
+echo "--- END DEBUG ---"
+
+exec nginx -g 'daemon off;'
