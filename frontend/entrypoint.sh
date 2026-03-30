ce37df12310461c102a2fb5b1efdf79f4926c7ae
wassabi-glitch <ahrorabrorov26@gmail.com>
2026-03-30 05:31:28 +0500

    fix: final industrial-strength absolute fix

--- frontend/entrypoint.sh
+++ frontend/entrypoint.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
-# BUILD ID: 1774825109354 - GHOST BUSTER 3000
-echo "--- DEBUG: Current Nginx Config ---"
+# --- FINAL DEBUG START ---
+echo "--- CURRENT NGINX CONFIG ---"
+cat /etc/nginx/conf.d/default.conf
+
+# Manual replacement of industry-grade symbols
+# Using | as a delimiter for safety
+sed -i "s|__REPLACE_ME_WITH_API_URL__|$API_URL|g" /etc/nginx/conf.d/default.conf
+sed -i "s|__REPLACE_ME_WITH_API_HOST__|$API_HOST|g" /etc/nginx/conf.d/default.conf
+
+echo "--- MODIFIED NGINX CONFIG ---"
 cat /etc/nginx/conf.d/default.conf
 echo "--- END DEBUG ---"
 
-# Manual replacement of placeholders
-# We use | as a delimiter because the URL contains /
-sed -i "s|__API_URL__|$API_URL|g" /etc/nginx/conf.d/default.conf
-sed -i "s|__API_HOST__|$API_HOST|g" /etc/nginx/conf.d/default.conf
-
-echo "--- DEBUG: Modified Nginx Config ---"
-cat /etc/nginx/conf.d/default.conf
-echo "--- END DEBUG ---"
-
+# Start Nginx
 exec nginx -g 'daemon off;'
